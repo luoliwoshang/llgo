1cd89007ca7d471757469bfd3deb804c7946527e
luoliwoshang <2643523683@qq.com>
2024-05-30 17:40:24 +0800

    test:cl funcCall #9

--- cl/compile_single_test.go
+++ cl/compile_single_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/goplus/llgo/cl/cltest"
 )
 
+// 测试普通包下的函数定义，包级别的初始化
 func TestApkg(t *testing.T) {
 	cltest.FromFolder(t, "apkg", "./_testdata/apkg", false)
+}
+
+// 测试Main包下的函数定义，调用，初始化（main包的处理）
+func TestFnCall(t *testing.T) {
+	cltest.FromFolder(t, "fncall", "./_testdata/fncall", false)
 }
