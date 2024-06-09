b70b8a089abbe7ef5c839715ad9b9df0e4cba286
luoliwoshang <2643523683@qq.com>
2024-06-09 20:49:08 +0800

    #9

--- cl/compile_single_test.go
+++ cl/compile_single_test.go
@@ -6,6 +6,16 @@ import (
 	"github.com/goplus/llgo/cl/cltest"
 )
 
+// 测试初始化全局变量
+func TestVarInit(t *testing.T) {
+	cltest.FromFolder(t, "varinit", "./_testdata/varinit", false)
+}
+
+// 测试Untyped变量
+func TestUntyped(t *testing.T) {
+	cltest.FromFolder(t, "untyped", "./_testdata/varinit", false)
+}
+
 // 测试普通包下的函数定义，包级别的初始化
 func TestApkg(t *testing.T) {
 	cltest.FromFolder(t, "apkg", "./_testdata/apkg", false)
@@ -29,4 +39,9 @@ func TestMethod(t *testing.T) {
 // TODO: 了解这里的print是否可以使用 测试runtime c.printf
 func TestPrintf(t *testing.T) {
 	cltest.FromFolder(t, "printf", "./_testdata/printf", false)
+}
+
+// 测试utf8，内置的runtime println
+func TestUtf8(t *testing.T) {
+	cltest.FromFolder(t, "utf8", "./_testdata/utf8", false)
 }
