a0e3e69fafbc9b320c55c66df99fa1658e7c9d21
luoliwoshang <2643523683@qq.com>
2024-07-31 00:03:28 +0800

    更简单的错误复现,去除不必要的结构,以及函数层级

--- chore/_xtool/llcppsymg/bugtest/test.go
+++ chore/_xtool/llcppsymg/bugtest/test.go
@@ -1,62 +1,35 @@
 package main
 
-import (
-	"errors"
-)
-
-// 模拟外部库
-type cjson struct{}
-
-func (c *cjson) Array() *cjson { return &cjson{} }
-func (c *cjson) Delete()       {}
-
-var cjsonLib = &cjson{}
-
-type SymbolInfo struct {
-	Mangle string
-	CPP    string
-	Go     string
-}
+import "errors"
 
 func main() {
-	err := processSymbols()
+	syms := []string{"sym1"}
+	m := make(map[string]bool)
+
+	err := errors.New("failed to read symbol table")
+
+	// 正常运行
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// 错误运行（取消注释以模拟错误）
 	check(err)
+
+	for i := range syms {
+		if _, ok := m[syms[i]]; ok {
+			println(syms[i])
+		}
+	}
+
+	defer println("bye")
+
+	for _, sym := range syms {
+		_ = sym
+	}
 }
 
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func processSymbols() error {
-	symbols := []SymbolInfo{{Mangle: "sym1", CPP: "Class::func1", Go: ""}}
-	err := genSymbolTableFile(symbols)
-	return err // 这里不使用 check，让错误传播到上层
-}
-
-func genSymbolTableFile(symbolInfos []SymbolInfo) error {
-	existingSymbols := make(map[string]SymbolInfo)
-
-	err := errors.New("failed to read symbol table file")
-	// 正常运行
-	// if err != nil {
-	// 	return err
-	// }
-	// 错误运行（取消注释以模拟错误）
-	check(err)
-	for i := range symbolInfos {
-		if existingSymbol, exists := existingSymbols[symbolInfos[i].Mangle]; exists {
-			symbolInfos[i].Go = existingSymbol.Go
-		}
-	}
-
-	root := cjsonLib.Array()
-	defer root.Delete()
-
-	for _, symbol := range symbolInfos {
-		_ = symbol
-	}
-
-	return nil
 }
