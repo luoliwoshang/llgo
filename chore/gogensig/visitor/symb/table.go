c15f1783fdb3ad45e6a63ded36762286566d4473
tsingbx <tsingbx@gmail.com>
2024-09-13 20:50:59 +0800

    fix TestUnmarshalAnyNode fail

--- chore/gogensig/visitor/symb/table.go
+++ chore/gogensig/visitor/symb/table.go
@@ -13,14 +13,14 @@ type CppNameType string
 
 type GoNameType string
 
-type SymbolEntry struct {
+type symbolntry struct {
 	MangleName MangleNameType `json:"mangle"`
 	CppName    CppNameType    `json:"c++"`
 	GoName     GoNameType     `json:"go"`
 }
 
 type SymbolTable struct {
-	t map[MangleNameType]SymbolEntry
+	t map[MangleNameType]symbolntry
 }
 
 func NewSymbolTable(filePath string) (*SymbolTable, error) {
@@ -28,20 +28,20 @@ func NewSymbolTable(filePath string) (*SymbolTable, error) {
 	if err != nil {
 		return nil, err
 	}
-	var symbs []SymbolEntry
+	var symbs []symbolntry
 	err = json.Unmarshal(bytes, &symbs)
 	if err != nil {
 		return nil, err
 	}
 	var symbolTable SymbolTable
-	symbolTable.t = make(map[MangleNameType]SymbolEntry)
+	symbolTable.t = make(map[MangleNameType]symbolntry)
 	for _, symb := range symbs {
 		symbolTable.t[symb.MangleName] = symb
 	}
 	return &symbolTable, nil
 }
 
-func (t *SymbolTable) LookupSymbol(name MangleNameType) (*SymbolEntry, error) {
+func (t *SymbolTable) LookupSymbol(name MangleNameType) (*symbolntry, error) {
 	symbol, ok := t.t[name]
 	if ok {
 		return &symbol, nil
